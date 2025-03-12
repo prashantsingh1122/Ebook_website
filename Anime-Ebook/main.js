1d9d74a4adb768d63e4491442d5d0597a2771c63
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-03-13 04:57:07 +0530

    imges for slider

--- Anime-Ebook/main.js
+++ Anime-Ebook/main.js
@@ -13,7 +13,7 @@
             spaceBetween: 30,
             centeredSlides: true,
             autoplay: {
-                delay: 7500,
+                delay: 1000,
                 disableOnInteraction: false,
             },
             pagination: {
